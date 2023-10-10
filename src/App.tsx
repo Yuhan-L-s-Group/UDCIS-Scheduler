a62f69ab6e1ec584ef94afd32106098617a07158
ZiyiZhou <alwinzzy@udel.edu>
2023-10-09 22:17:45 -0400

    add name

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">Yuhan L‘s Group</header>
-            <p>Group Members: Yuhan Lin Priyanka Chaudhuri Zonglin Wu</p>
+            <p>Group Members: Yuhan Lin, Priyanka, Chaudhuri, Zonglin Wu, Ziyi Zhou</p>
         </div>
     );
 }
