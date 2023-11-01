68130a9bbf9abb58062dea7fe6b4a27b5c3252d8
ZONGLIN <ZONGLIN@UDEL.EDU>
2023-11-01 13:04:19 -0400

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-
+import CoursesTable from "./components/courseTable";
+import { TwoModals } from "./components/TwoModals";
 function App(): JSX.Element {
     return (
         <div className="App">
@@ -9,6 +10,11 @@ function App(): JSX.Element {
                 Group Members: Yuhan Lin, Priyanka Chaudhuri, Zonglin Wu, Ziyi
                 Zhou, Henry Grant, Thern Diallo
             </p>
+            <hr></hr>
+            <TwoModals></TwoModals>
+            <hr></hr>
+
+            <hr></hr>
         </div>
     );
 }
