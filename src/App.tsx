8c4850df922623470e6e97d73ea25b090cf69ca5
Yuhan Lin <yuhanlin@udel.edu>
2023-11-02 16:53:08 -0400

    Add semester, but haven't display in main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import { SwitchPlan } from "./components/SwitchPlan";
+import { AddSemesterModal } from "./components/SemesterModal";
+import { Season, Semester } from "./interfaces/semester";
+import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
+    const [showModal, setShowModal] = useState(false);
+    const [semester, setSemester] = useState<Semester[]>([]);
+
+    const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+
+    function addNewSemester(year: number, season: Season) {
+        setSemester((prevSemester) => [
+            ...prevSemester,
+            {
+                season: season,
+                year: year,
+                courses: []
+            }
+        ]);
+    }
+
+    function print() {
+        console.log(semester);
+    }
+
     return (
         <div className="App">
             <header className="App-header">Yuhan L‘s Group</header>
@@ -12,6 +36,15 @@ function App(): JSX.Element {
                 Zhou, Henry Grant, Thern Diallo
             </p>
             <SwitchPlan></SwitchPlan>
+            <Button className="add_btn" onClick={handleShowModal}>
+                Add New Semester
+            </Button>
+            <AddSemesterModal
+                show={showModal}
+                handleClose={handleCloseModal}
+                addSemester={addNewSemester}
+            ></AddSemesterModal>
+            <Button onClick={print}>Print</Button>
         </div>
     );
 }
