c867cabe7748b02dabc1f8eea173c1c3c4452034
Yuhan Lin <yuhanlin@udel.edu>
2023-11-02 17:28:43 -0400

    Made an intro modal.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { IntroModal } from "./components/IntroModal";
 
 function App(): JSX.Element {
+    const [showIntro, setShowIntro] = useState<boolean>(true);
+    const handleClose = () => setShowIntro(false);
+
     return (
         <div className="App">
+            <IntroModal show={showIntro} handleClose={handleClose}></IntroModal>
             <header className="App-header">Yuhan L‘s Group</header>
             <p>
                 Group Members: Yuhan Lin, Priyanka Chaudhuri, Zonglin Wu, Ziyi
---
+++ src/components/IntroModal.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+export function IntroModal({
+    show,
+    handleClose
+}: {
+    show: boolean;
+    handleClose: () => void;
+}) {
+    return (
+        <div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Welcome to the UD CIS Scheduler</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    This website allows you to create your own computer science
+                    major degree plan at University of Delaware. The menu on the
+                    left shows all the required courses for the major, and you
+                    can easily edit them. You can also add custom courses if
+                    there are none that meet your needs. You can switch between
+                    concentrations in the drop-down menu at the top to make the
+                    list of courses more relevant to your concentration needs.
+                    Have fun!
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        OK!
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+}
