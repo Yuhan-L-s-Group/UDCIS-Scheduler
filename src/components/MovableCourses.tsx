2ddc8fc602a1f0a7e920be2a19cf4d294bf24c33
Priyanka Chaudhuri <priankac@udel.edu>
2023-11-19 22:30:36 -0500

    filename

--- src/components/MovableCourses.tsx
+++ src/components/MovableCourses.tsx
@@ -9,9 +9,86 @@ interface MovableCourse {
 }
 
 const MovableCourses = ({ semesters }: MovableCourse) => {
-    const [selectedCourse, setSelectedCourse] = useState<Course | null>(null);
+    const [selectCourse, setSelectedCourse] = useState<Course | null>(null);
+    const [selectSemester, setSelectedSemester] = useState<Semester | null>(
+        null
+    );
 
-    const handleCourseSelection = (course: Course) => {
+    const CourseSelection = (course: Course) => {
         setSelectedCourse(course);
     };
+
+    const MoveCourse = (semester: Semester) => {
+        if (selectCourse && selectSemester) {
+            const updatedSemesters = semesters.map((sem) => {
+                if (sem === selectSemester) {
+                    const updatedCourses = sem.courses.filter(
+                        (course) => course !== selectCourse
+                    );
+                    return { ...sem, courses: updatedCourses };
+                }
+                return sem;
+            });
+
+            const toChosenSemester = updatedSemesters.find(
+                (sem) => sem === semester
+            );
+            if (toChosenSemester) {
+                const updatedCourses = [
+                    ...toChosenSemester.courses,
+                    selectCourse
+                ];
+                const updatedSemester = {
+                    ...toChosenSemester,
+                    courses: updatedCourses
+                };
+
+                const updatedSemestersList = updatedSemesters.map((sem) =>
+                    sem === updatedSemester ? updatedSemester : sem
+                );
+            }
+
+            setSelectedCourse(null);
+            setSelectedSemester(null);
+        }
+    };
+
+    const ToDeleteCourse = (course: Course) => {
+        if (selectSemester) {
+            const updatedSemesters = semesters.map((semester) => {
+                if (semester === selectSemester) {
+                    const updatedCourses = semester.courses.filter(
+                        (c) => c !== course
+                    );
+                    return { ...semester, courses: updatedCourses };
+                }
+                return semester;
+            });
+
+            setSelectedCourse(null);
+            setSelectedSemester(null);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Move Courses</h2>
+            <div>
+                {semesters.map((semester) => (
+                    <div key={semester.season + "-" + semester.year}>
+                        <SemesterDisplay
+                            semester={semester}
+                            deleteSemester={() => ToDeleteCourse}
+                            //                          onClick={CourseSelection}
+                        />
+                        <Button onClick={() => MoveCourse(semester)}>
+                            Move Selected Course Here
+                        </Button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
+
+export default MovableCourses;
