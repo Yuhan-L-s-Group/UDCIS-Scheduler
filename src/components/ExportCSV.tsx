acc0add12d9dc0d5b32807f4947cc9ee781b12e5
Henry Grant <Henryg@udel.edu>
2023-11-27 13:15:53 -0500

    almost done with export csv file

---
+++ src/components/ExportCSV.tsx
@@ -0,0 +1,32 @@
+import Papa from 'papaparse';
+import degreePlan from '/.degreePlan';
+import React, { useState } from "react";
+
+const degreePlanData = () => {
+  const [degreePlan, setDegreePlan] = useState([]); ;
+
+
+const csv = Papa.unparse(degreePlanData);
+
+const handleExportCSV = () => {
+    // Converting degreePlan data to CSV format
+    const csv = degreePlan.map(course => Object.values(course).join(',')).join('\n');
+
+const blob = new Blob([csv], { type: 'text/csv' });
+const url = window.URL.createObjectURL(blob);
+
+const link = document.createElement('a');
+link.href = url;
+link.setAttribute('download', 'degree_plan.csv');
+document.body.appendChild(link);
+link.click();
+document.body.removeChild(link);
+
+return (
+    <div>
+      <button onClick={handleExportCSV}>Export as CSV</button>
+    </div>
+  );
+};
+
+export default degreePlan;
