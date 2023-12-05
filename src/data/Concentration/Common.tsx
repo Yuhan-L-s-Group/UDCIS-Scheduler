6aeaf48e4893cea8a9e4caaa7154da56e3e3c433
Yuhan Lin <yuhanlin@udel.edu>
2023-12-05 00:03:59 -0500

    Sovled displaying problem

--- src/data/Concentration/Common.tsx
+++ src/data/Concentration/Common.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import { DegreePlan } from "../../interfaces/degreePlan";
 import all from "../RequiredCourses/FitsAll.json";
 import { Course } from "../../interfaces/course";
@@ -18,38 +17,46 @@ export const Common = ({ degreePlan }: { degreePlan: DegreePlan }) => {
         return outPut as Course[];
     }
 
-    let HTMLoutPut = "";
-    all.forEach((require) => {
+    const requirementsOutput = all.map((require) => {
         const result = findSameCourse(allCourses, require.courses);
+
         if (require.type === "courses") {
-            if (result.length === require.number) {
-                HTMLoutPut += `<h4>${require.name}✔️</h4>`;
-                HTMLoutPut += `<span style={{ color: "green" }}><strong>${result.length}/${require.number}: </strong></span>`;
-            } else {
-                HTMLoutPut += `<h4>${require.name}❌</h4>`;
-                HTMLoutPut += `<span style={{ color: "red" }}><strong>${result.length}/${require.number}: </strong></span>`;
-            }
+            const isFulfilled = result.length === require.number;
+            return (
+                <div key={require.name}>
+                    <h4>
+                        {require.name}
+                        {isFulfilled ? "✔️" : "❌"}
+                    </h4>
+                    <span style={{ color: isFulfilled ? "green" : "red" }}>
+                        <strong>
+                            {result.length}/{require.number}:{" "}
+                        </strong>
+                    </span>
+                </div>
+            );
         } else {
-            //if that require is about credit
             const totalCredits: number = result.reduce(
                 (acc, course) => acc + parseInt(course.credits),
                 0
             );
-            if (totalCredits >= require.number) {
-                HTMLoutPut += `<h4>${require.name}✔️</h4>`;
-                HTMLoutPut += `<span style={{ color: "green" }}><strong>${totalCredits}/${require.number}: </strong></span>`;
-            } else {
-                HTMLoutPut += `<h4>${require.name}❌</h4>`;
-                HTMLoutPut += `<span style={{ color: "red" }}><strong>${totalCredits}/${require.number}: </strong></span>`;
-            }
+            const isFulfilled = totalCredits >= require.number;
+            return (
+                <div key={require.name}>
+                    <h4>
+                        {require.name}
+                        {isFulfilled ? "✔️" : "❌"}
+                    </h4>
+                    <span style={{ color: isFulfilled ? "green" : "red" }}>
+                        <strong>
+                            {totalCredits}/{require.number}:{" "}
+                        </strong>
+                    </span>
+                </div>
+            );
         }
     });
 
     //✔️❌
-    return (
-        <div>
-            {HTMLoutPut}
-            <Button onClick={print}>Print</Button>
-        </div>
-    );
+    return <div>{requirementsOutput}</div>;
 };
