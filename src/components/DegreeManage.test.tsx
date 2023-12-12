4a7987a86001ba0d3f0a146d90d7467b7c1c1d6a
ZONGLIN <ZONGLIN@UDEL.EDU>
2023-12-12 05:50:17 -0500

    test file

---
+++ src/components/DegreeManage.test.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import DegreeManage from "./DegreeManage";
+import { DegreePlan, Concentration } from "../interfaces/degreePlan";
+
+describe("DegreeManage Component", () => {
+    const mockSetisDegreePlanOpen = jest.fn();
+    const mockSetDegreeList = jest.fn();
+    const degreeList: DegreePlan[] = []; // Assuming degreeList is an array of DegreePlan
+
+    const renderComponent = () =>
+        render(
+            <DegreeManage
+                setisDegreePlanOpen={mockSetisDegreePlanOpen}
+                degreeList={degreeList}
+                setDegreeList={mockSetDegreeList}
+            />
+        );
+    /* can't pass
+    test("renders input fields and save button", () => {
+        const { getByTestId, getByText, debug } = renderComponent();
+        expect(getByTestId("degree-plan-name-input")).toBeInTheDocument();
+        expect(getByTestId("concentration-select")).toBeInTheDocument();
+        expect(getByText("Save")).toBeInTheDocument();
+    });
+*/
+
+    /*  can't pass
+    test("allows the user to enter a degree plan name and select a concentration", () => {
+        const { getByTestId } = renderComponent();
+        const inputName = getByTestId(
+            "degree-plan-name-input"
+        ) as HTMLInputElement;
+        const selectConcentration = getByTestId(
+            "concentration-select"
+        ) as HTMLSelectElement;
+
+        fireEvent.change(inputName, { target: { value: "Plan A" } });
+        fireEvent.change(selectConcentration, {
+            target: { value: "Bachelor of Science" }
+        });
+
+        expect(inputName.value).toBe("Plan A");
+        expect(selectConcentration.value).toBe("Bachelor of Science");
+    });
+*/
+
+    test("handles save button click", () => {
+        const { getByText } = renderComponent();
+        fireEvent.click(getByText("Save"));
+        expect(mockSetDegreeList).toHaveBeenCalled();
+        expect(mockSetisDegreePlanOpen).toHaveBeenCalledWith(false);
+    });
+});
