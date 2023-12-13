e4ca8f715c6237ae5069ada134531337a89d0a35
thernd302 <thernd@udel.edu>
2023-12-12 22:26:48 -0500

    update again

--- src/components/ImportCSV.tsx
+++ src/components/ImportCSV.tsx
@@ -1,55 +1,87 @@
-import React, { ChangeEvent, useState } from "react";
-import { Semester } from "../interfaces/semester";
-import { DegreePlan } from "../interfaces/degreePlan";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useState } from "react";
+import { Concentration, DegreePlan } from "../interfaces/degreePlan";
 
 interface ImportCSVProps {
-    onImport: DegreePlan[];
+    onImport: (degreeList: DegreePlan[]) => void;
 }
 
 export const ImportCSV: React.FC<ImportCSVProps> = ({ onImport }) => {
-    const handleImportCSV = (e: ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files?.[0];
+    const handleImportCSV = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
 
         if (file) {
-            const fileReader = new FileReader();
-
-            fileReader.onload = (event) => {
-                if (event && event.target) {
-                    const csvText = event.target.result as string;
-
-                    // Parse CSV data and convert it to the desired format
-                    const csvData: {
-                        Plan: string;
-                        Semester: string;
-                        CourseCode: string;
-                        CourseName: string;
-                        Credits: string;
-                        Concentration: string;
-                    }[] = [
-                        {
-                            Plan: "Plan",
-                            Semester: "Season",
-                            CourseCode: "Course Code",
-                            CourseName: "Course Name",
-                            Credits: "Credits",
-                            Concentration: "Concentration"
-                        }
-                    ];
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                try {
+                    const csvData = e.target?.result as string;
+                    const degreeList = parseCSVData(csvData);
+                    onImport(degreeList);
+                } catch (error) {
+                    console.error("Error parsing CSV:", error);
                 }
             };
-
-            fileReader.readAsText(file);
+            reader.readAsText(file);
         }
+    };
+
+    const parseCSVData = (csvData: string): DegreePlan[] => {
+        const lines = csvData.split("\n");
+
+        // Assuming the first row contains headers, skip it
+        const headers = lines[0].split(",");
+
+        const degreePlans: DegreePlan[] = [];
+
+        for (let i = 1; i < lines.length; i++) {
+            const values = lines[i].split(",");
+            if (values.length !== headers.length) {
+                console.error(`Skipping invalid CSV row at index ${i}`);
+                continue;
+            }
+
+            const degreePlan: DegreePlan = {
+                Plan: values[headers.indexOf("Plan")],
+                concentration: values[
+                    headers.indexOf("Concentration")
+                ] as Concentration,
+                semesters: [
+                    {
+                        Season: values[headers.indexOf("Semester")],
+                        year: parseInt(
+                            values[headers.indexOf("Semester") + 1],
+                            10
+                        ),
+                        courses: [
+                            {
+                                code: values[headers.indexOf("CourseCode")],
+                                name: values[headers.indexOf("CourseName")],
+                                credits: values[headers.indexOf("Credits")]
+                            }
+                        ]
+                    }
+                ]
+            };
+
+            // Check if the degree plan already exists, and if so, add semesters and courses
+            const existingDegreePlan = degreePlans.find(
+                (dp) =>
+                    dp.name === degreePlan.name &&
+                    dp.concentration === degreePlan.concentration
+            );
+            if (existingDegreePlan) {
+                existingDegreePlan.semesters.push(...degreePlan.semesters);
+            } else {
+                degreePlans.push(degreePlan);
+            }
+        }
+
+        return degreePlans;
     };
 
     return (
         <div>
-            <input
-                type="file"
-                accept=".csv"
-                onChange={handleImportCSV}
-                className="ImportCSVInput"
-            />
+            <input type="file" accept=".csv" onChange={handleImportCSV} />
         </div>
     );
 };
